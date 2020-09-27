05c36a927bdca10b2b70e90d5d988b14843ef260
ElhourefAissam <aissamelhouref@gmail.com>
2020-09-27 17:59:55 +0100

    c

--- app/Employe.php
+++ app/Employe.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employe extends Model
 {
+    protected $fillable = ['NomFr','NomAr','CNI','DNS','Sexe','StatutFamilierFr',
+                            'StatutFamilierAr','Telephone','AdressFr','AdressAr','Picture'];
+                            
     public function contracts()
     {
         return $this->hasMany('App\contract');
